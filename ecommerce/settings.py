7fb30f428a8f01d37db387d9fbaf4db5747b6d86
MelBiggs <melissabiggs16@gmail.com>
2020-02-17 16:41:36 +0000

    Set up cart, checkout, payment with stripe

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'products',
     'accounts',
+    'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -127,8 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
--- products/views.py
+++ products/views.py
@@ -3,5 +3,5 @@ from .models import Product
 
 # Create your views here.
 def all_products(request):
-    products = Product.objects.all
+    products = Product.objects.all()
     return render(request, "products.html", {"products": products})
